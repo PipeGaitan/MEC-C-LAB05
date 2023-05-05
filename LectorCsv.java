db832028b9f0c54b66f4d9396c49d9b49e34ba97
PipeGaitan <123768760+PipeGaitan@users.noreply.github.com>
2023-05-05 10:20:49 -0500

    Add files via upload
    Buenos dias profesor a la hora de querer subir el archivo como lo hemos hecho hasta el momento(sin comprimir) no me deja subir el archivo csv porque es muy pesado asi que esta en este momento sin ese archivo adentro

---
+++ LectorCsv.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class LectorCsv {
+   private String codigo;
+   private String grupoEdad;
+   private String nivelAcademico;
+   private String genero;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getGrupoEdad() {
+        return grupoEdad;
+    }
+
+    public void setGrupoEdad(String grupoEdad) {
+        this.grupoEdad = grupoEdad;
+    }
+
+    public String getNivelAcademico() {
+        return nivelAcademico;
+    }
+
+    public void setNivelAcademico(String nivelAcademico) {
+        this.nivelAcademico = nivelAcademico;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
+}
